2bb691e5e98370fc8927f2ea2c78e91ba82b8a3b
Saisri24 <69609144+Saisri24@users.noreply.github.com>
2021-11-30 17:29:44 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <link rel="stylesheet" href="style.css">
+  <link rel="javascript" href="script.js">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <title> CSP Project by Saisri, Edlyn, Anisha </title>
+</head>
+<body>
 
+</body>
+ 
+</html>
