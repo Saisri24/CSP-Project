e6178df09be765e473ec49ef60d05ab5e18edeed
Saisri24 <69609144+Saisri24@users.noreply.github.com>
2021-12-19 23:54:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function bigImg(x) {
+    x.style.height = "310px";
+    x.style.width = "370px";
+  }
+  
+function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "200px";
+}
+
+function bigImg1(x) {
+    x.style.height = "250px";
+    x.style.width = "400px";
+  }
+  
+function normalImg1(x) {
+    x.style.height = "100px";
+    x.style.width = "200px";
+}
+  
 
